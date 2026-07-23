592f2170f7791bfa0639f19833e489fd7b30c68e
taduchuy2603 <taduchuy2603@gmail.com>
2026-07-23 15:27:01 +0700

    Feature (us-m-07): Fix code format

--- src/main/java/com/restaurant/crm/modules/erp/inventory/service/impl/IngredientServiceImpl.java
+++ src/main/java/com/restaurant/crm/modules/erp/inventory/service/impl/IngredientServiceImpl.java
@@ -24,33 +24,52 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class IngredientServiceImpl implements IngredientService {
 
     IngredientRepository ingredientRepository;
-
     IngredientCategoryRepository ingredientCategoryRepository;
-
     OrganizationBranchRepository organizationBranchRepository;
-
     IngredientMapper ingredientMapper;
-
 
     @Override
     @Transactional
-    public IngredientResponse createIngredient(CreateIngredientRequest request) {
+    public IngredientResponse createIngredient(
+            CreateIngredientRequest request
+    ) {
 
-        OrganizationBranch branch = organizationBranchRepository.findById(request.getBranchId()).orElseThrow(() -> new AppException(ErrorCode.ORGANIZATION_BRANCH_NOT_FOUND));
-        IngredientCategory category = ingredientCategoryRepository.findById(request.getIngredientCategoryId()).orElseThrow(() -> new AppException(ErrorCode.INGREDIENT_CATEGORY_NOT_FOUND));
+        OrganizationBranch branch =
+            organizationBranchRepository.findById(request.getBranchId())
+                .orElseThrow(() ->
+                    new AppException(
+                        ErrorCode.ORGANIZATION_BRANCH_NOT_FOUND
+                    )
+                );
 
-        if (ingredientRepository.existsByBranchIdAndIngredientName(request.getBranchId(), request.getIngredientName())) {
-            throw new AppException(ErrorCode.INGREDIENT_EXISTS);
+        IngredientCategory category =
+            ingredientCategoryRepository.findById(
+                request.getIngredientCategoryId()
+                )
+                .orElseThrow(() ->
+                        new AppException(
+                            ErrorCode.INGREDIENT_CATEGORY_NOT_FOUND
+                        )
+                );
+
+        if (ingredientRepository.existsByBranchIdAndIngredientName(
+                request.getBranchId(),
+                request.getIngredientName()
+        )) {
+            throw new AppException(
+                    ErrorCode.INGREDIENT_EXISTS
+            );
         }
 
-        Ingredient ingredient = ingredientMapper.toIngredient(request);
+        Ingredient ingredient =
+                ingredientMapper.toIngredient(request);
+
         ingredient.setBranch(branch);
         ingredient.setIngredientCategory(category);
         ingredient = ingredientRepository.save(ingredient);
@@ -58,60 +77,164 @@ public class IngredientServiceImpl implements IngredientService {
         return ingredientMapper.toIngredientResponse(ingredient);
     }
 
-
     @Override
     @Transactional(readOnly = true)
-    public IngredientResponse getIngredientById(String id) {
-        Ingredient ingredient = ingredientRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.INGREDIENT_NOT_FOUND));
+    public IngredientResponse getIngredientById(
+            String id
+    ) {
+
+        Ingredient ingredient =
+            ingredientRepository.findById(id)
+                .orElseThrow(() ->
+                    new AppException(
+                        ErrorCode.INGREDIENT_NOT_FOUND
+                    )
+                );
+
         return ingredientMapper.toIngredientResponse(ingredient);
     }
 
+    @Override
+    @Transactional(readOnly = true)
+    public PagingResponse<IngredientResponse> getIngredientsByBranch(
+            String branchId,
+            int page,
+            int size
+    ) {
+
+        Pageable pageable =
+            PageRequest.of(
+                page - GlobalVariableConstant.PAGE_SIZE_INDEX,
+                size
+            );
+
+        Page<Ingredient> ingredientPage =
+            ingredientRepository.findByBranchId(
+                branchId,
+                pageable
+            );
+
+
+        return PagingResponse.<IngredientResponse>builder()
+            .currentPage(page)
+            .pageSize(ingredientPage.getSize())
+            .totalPages(ingredientPage.getTotalPages())
+            .totalElement(ingredientPage.getTotalElements())
+            .data(
+                ingredientPage.getContent()
+                    .stream()
+                    .map(ingredientMapper::toIngredientResponse)
+                    .toList()
+            )
+            .build();
+    }
 
     @Override
     @Transactional(readOnly = true)
-    public PagingResponse<IngredientResponse> getIngredientsByBranch(String branchId, int page, int size) {
-        Pageable pageable = PageRequest.of(page - GlobalVariableConstant.PAGE_SIZE_INDEX, size);
-        Page<Ingredient> ingredientPage = ingredientRepository.findByBranchId(branchId, pageable);
+    public PagingResponse<IngredientResponse> searchIngredients(
+        String branchId,
+        String ingredientName,
+        int page,
+        int size
+    ) {
 
-        return PagingResponse.<IngredientResponse>builder().currentPage(page).pageSize(ingredientPage.getSize()).totalPages(ingredientPage.getTotalPages()).totalElement(ingredientPage.getTotalElements()).data(ingredientPage.getContent().stream().map(ingredientMapper::toIngredientResponse).toList()).build();
+        Pageable pageable =
+            PageRequest.of(
+                page - GlobalVariableConstant.PAGE_SIZE_INDEX,
+                size
+            );
+
+        Page<Ingredient> ingredientPage =
+            ingredientRepository
+                .findByBranchIdAndIngredientNameContainingIgnoreCase(
+                    branchId,
+                    ingredientName,
+                    pageable
+                );
+
+        return PagingResponse.<IngredientResponse>builder()
+            .currentPage(page)
+            .pageSize(ingredientPage.getSize())
+            .totalPages(ingredientPage.getTotalPages())
+            .totalElement(ingredientPage.getTotalElements())
+            .data(
+                ingredientPage.getContent()
+                    .stream()
+                    .map(ingredientMapper::toIngredientResponse)
+                    .toList()
+            )
+            .build();
     }
-
-
-    @Override
-    @Transactional(readOnly = true)
-    public PagingResponse<IngredientResponse> searchIngredients(String branchId, String ingredientName, int page, int size) {
-        Pageable pageable = PageRequest.of(page - GlobalVariableConstant.PAGE_SIZE_INDEX, size);
-        Page<Ingredient> ingredientPage = ingredientRepository.findByBranchIdAndIngredientNameContainingIgnoreCase(branchId, ingredientName, pageable);
-
-        return PagingResponse.<IngredientResponse>builder().currentPage(page).pageSize(ingredientPage.getSize()).totalPages(ingredientPage.getTotalPages()).totalElement(ingredientPage.getTotalElements()).data(ingredientPage.getContent().stream().map(ingredientMapper::toIngredientResponse).toList()).build();
-    }
-
 
     @Override
     @Transactional
-    public IngredientResponse updateIngredient(String id, UpdateIngredientRequest request) {
-        Ingredient ingredient = ingredientRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.INGREDIENT_NOT_FOUND));
+    public IngredientResponse updateIngredient(
+            String id,
+            UpdateIngredientRequest request
+    ) {
+
+        Ingredient ingredient =
+            ingredientRepository.findById(id)
+                .orElseThrow(() ->
+                        new AppException(
+                            ErrorCode.INGREDIENT_NOT_FOUND
+                        )
+                );
 
         if (request.getIngredientCategoryId() != null) {
-            IngredientCategory category = ingredientCategoryRepository.findById(request.getIngredientCategoryId()).orElseThrow(() -> new AppException(ErrorCode.INGREDIENT_CATEGORY_NOT_FOUND));
+            IngredientCategory category =
+                ingredientCategoryRepository.findById(
+                    request.getIngredientCategoryId()
+                    )
+                    .orElseThrow(() ->
+                            new AppException(
+                                ErrorCode.INGREDIENT_CATEGORY_NOT_FOUND
+                            )
+                    );
             ingredient.setIngredientCategory(category);
         }
 
-        if (request.getIngredientName() != null && !request.getIngredientName().equals(ingredient.getIngredientName()) && ingredientRepository.existsByBranchIdAndIngredientName(ingredient.getBranch().getId(), request.getIngredientName())) {
-            throw new AppException(ErrorCode.INGREDIENT_EXISTS);
+
+        if (request.getIngredientName() != null
+            && !request.getIngredientName()
+            .equals(ingredient.getIngredientName())
+            && ingredientRepository.existsByBranchIdAndIngredientName(
+                ingredient.getBranch().getId(),
+            request.getIngredientName()
+        )) {
+
+            throw new AppException(
+                ErrorCode.INGREDIENT_EXISTS
+            );
         }
 
-        ingredientMapper.updateIngredient(request, ingredient);
-        ingredient = ingredientRepository.save(ingredient);
+        ingredientMapper.updateIngredient(
+            request,
+            ingredient
+        );
 
-        return ingredientMapper.toIngredientResponse(ingredient);
+        ingredient =
+            ingredientRepository.save(ingredient);
+
+        return ingredientMapper.toIngredientResponse(
+            ingredient
+        );
     }
-
 
     @Override
     @Transactional
-    public void deleteIngredient(String id) {
-        Ingredient ingredient = ingredientRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.INGREDIENT_NOT_FOUND));
+    public void deleteIngredient(
+            String id
+    ) {
+
+        Ingredient ingredient =
+            ingredientRepository.findById(id)
+                .orElseThrow(() ->
+                        new AppException(
+                            ErrorCode.INGREDIENT_NOT_FOUND
+                        )
+                );
+
         ingredientRepository.delete(ingredient);
     }
 }
