7036d9218b339eaf512eafe64073501b683b7fa9
catsocute <nguyenlenhat205@gmail.com>
2026-07-28 16:59:57 +0700

    fix pre authorize with role

--- src/main/java/com/restaurant/crm/modules/profile/service/interfaces/ProfileService.java
+++ src/main/java/com/restaurant/crm/modules/profile/service/interfaces/ProfileService.java
@@ -1,11 +1,17 @@
 package com.restaurant.crm.modules.profile.service.interfaces;
 
+import com.restaurant.crm.common.dto.request.PagingRequest;
+import com.restaurant.crm.common.dto.response.PagingResponse;
 import com.restaurant.crm.modules.profile.dto.response.UserProfileResponse;
 import com.restaurant.crm.modules.profile.dto.request.ProfileUpdateRequest;
 import com.restaurant.crm.modules.profile.dto.request.StaffProfileUpdateRequest;
 
 public interface ProfileService {
     UserProfileResponse getMyInfo();
+
+    PagingResponse<UserProfileResponse> getAll(PagingRequest request);
+
+    UserProfileResponse getById(String profileId);
 
     UserProfileResponse updateMyInfo(ProfileUpdateRequest request);
 
