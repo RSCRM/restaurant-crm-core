b24653cd4bcbf74e2a3112dd71ee2e90bde37e47
catsocute <nguyenlenhat205@gmail.com>
2026-08-03 11:37:09 +0700

    add search combo and products

--- src/main/java/com/restaurant/crm/modules/erp/menu/service/interfaces/ComboManagementService.java
+++ src/main/java/com/restaurant/crm/modules/erp/menu/service/interfaces/ComboManagementService.java
@@ -1,6 +1,9 @@
 package com.restaurant.crm.modules.erp.menu.service.interfaces;
 
+import com.restaurant.crm.common.dto.request.PagingRequest;
+import com.restaurant.crm.common.dto.response.PagingResponse;
 import com.restaurant.crm.modules.erp.menu.dto.request.ComboItemRequest;
+import com.restaurant.crm.modules.erp.menu.dto.request.ComboSearchRequest;
 import com.restaurant.crm.modules.erp.menu.dto.request.CreateComboRequest;
 import com.restaurant.crm.modules.erp.menu.dto.request.UpdateComboRequest;
 import com.restaurant.crm.modules.erp.menu.dto.response.ComboItemResponse;
@@ -14,6 +17,7 @@ public interface ComboManagementService {
     void delete(String id);
     List<ComboResponse> listByBranch(String branchId);
     ComboResponse get(String id);
+    PagingResponse<ComboResponse> searchCombos(ComboSearchRequest searchRequest, PagingRequest pagingRequest);
     ComboItemResponse addItem(String comboId, ComboItemRequest request);
     ComboItemResponse updateItem(String itemId, ComboItemRequest request);
     void deleteItem(String itemId);
