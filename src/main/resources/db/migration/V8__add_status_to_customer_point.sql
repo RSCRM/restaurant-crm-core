4bac892587c91cc48b6f0ba4bce77d72944922ff
DangKhoa <khoahuynh849@gmail.com>
2026-08-10 12:48:33 +0700

    fix: Manager permission assignment

--- src/main/resources/db/migration/V8__add_status_to_customer_point.sql
+++ src/main/resources/db/migration/V8__add_status_to_customer_point.sql
@@ -8,6 +8,12 @@ BEGIN
             WHERE table_schema = 'public'
               AND table_name = 'customer_point'
               AND column_name = 'restaurant_id'
+        ) AND NOT EXISTS (
+            SELECT 1
+            FROM information_schema.columns
+            WHERE table_schema = 'public'
+              AND table_name = 'customer_point'
+              AND column_name = 'organization_id'
         ) THEN
             ALTER TABLE customer_point RENAME COLUMN restaurant_id TO organization_id;
         END IF;
